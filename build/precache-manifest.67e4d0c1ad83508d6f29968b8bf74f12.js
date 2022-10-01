617af073c0fb01c56571b49b279412264e574dc9
Javier Díaz <3javidiaz@gmail.com>
2022-10-01 13:37:09 +0200

    Build commit

---
+++ build/precache-manifest.67e4d0c1ad83508d6f29968b8bf74f12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41f8e19d6cbd5bdee960",
+    "url": "/static/css/main.42047b1f.chunk.css"
+  },
+  {
+    "revision": "41f8e19d6cbd5bdee960",
+    "url": "/static/js/main.41f8e19d.chunk.js"
+  },
+  {
+    "revision": "f91cc19166e9df4b9f19",
+    "url": "/static/js/1.f91cc191.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0972a2009bc610fa071ae4518289dae3",
+    "url": "/index.html"
+  }
+];
