4564037cf68aa84481a67a8e47b3335ab3fc5263
Javier Díaz <3javidiaz@gmail.com>
2022-10-02 12:03:47 +0200

    Updates

---
+++ precache-manifest.b965b6de75b75620000eef12e107b1dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46ee2b589efaad2177ed",
+    "url": "/qrlizardgithub/static/css/main.e3cfd8c7.chunk.css"
+  },
+  {
+    "revision": "46ee2b589efaad2177ed",
+    "url": "/qrlizardgithub/static/js/main.46ee2b58.chunk.js"
+  },
+  {
+    "revision": "eae11029cc76aa824f8e",
+    "url": "/qrlizardgithub/static/js/1.eae11029.chunk.js"
+  },
+  {
+    "revision": "51f5e1b5ba5650830dac",
+    "url": "/qrlizardgithub/static/js/runtime~main.51f5e1b5.js"
+  },
+  {
+    "revision": "d0a61f8e2d8c656108d0813052566678",
+    "url": "/qrlizardgithub/index.html"
+  }
+];
